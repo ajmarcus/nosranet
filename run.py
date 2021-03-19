9c5a29c0a0dcd8857dcae8262e7ca675329d7645
Ariel Marcus <hi@arielmarcus.com>
2021-03-18 20:05:18 -0400

    hm

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     dropout_prob = [0.001, 0.4]
     for l in Label:
         for m in Model:
-            evaluate_baseline(label=l, name=m)
+            # evaluate_baseline(label=l, name=m)
             for nl in num_layers:
                 for d in dropout_prob:
                     evaluate(label=l, name=m, num_layers=nl, dropout_prob=d, epochs=100)
